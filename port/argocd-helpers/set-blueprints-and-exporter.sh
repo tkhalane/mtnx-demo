4e351829a49cd0ac66fc8f7c403c7d016e41b299
Tiisetso <tkhalane@gmail.com>
2023-07-22 12:26:21 +0200

    blueprint and exporter installation config

--- port/argocd-helpers/set-blueprints-and-exporter.sh
+++ port/argocd-helpers/set-blueprints-and-exporter.sh
@@ -1,7 +1,9 @@
-export CUSTOM_BP_PATH="https://raw.githubusercontent.com/port-labs/template-assets/main/kubernetes/blueprints/argo-blueprints.json"
-export CONFIG_YAML_URL="https://raw.githubusercontent.com/port-labs/template-assets/main/kubernetes/templates/argo-kubernetes_v1_config.yaml"
+# export CUSTOM_BP_PATH="https://raw.githubusercontent.com/port-labs/template-assets/main/kubernetes/blueprints/argo-blueprints.json"
+export CUSTOM_BP_PATH="https://raw.githubusercontent.com/tkhalane/mtnx-demo/main/port/argocd-helpers/argo-blueprints.json"
+# export CONFIG_YAML_URL="https://raw.githubusercontent.com/port-labs/template-assets/main/kubernetes/templates/argo-kubernetes_v1_config.yaml"
+export CONFIG_YAML_URL="https://raw.githubusercontent.com/tkhalane/mtnx-demo/main/port/argocd-helpers/exporter-config.yaml"
 
-export CLUSTER_NAME="my-cluster"
-export PORT_CLIENT_ID="my-port-client-id"
-export PORT_CLIENT_SECRET="my-port-client-secret"
+export CLUSTER_NAME="docker-desktop"
+# export PORT_CLIENT_ID="ds"
+# export PORT_CLIENT_SECRET="sd"
 curl -s https://raw.githubusercontent.com/port-labs/template-assets/main/kubernetes/install.sh | bash
