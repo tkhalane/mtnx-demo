eb5bebf4ec46520b14cf3cfbb9662fa3c2b04ff8
Tiisetso.Khalane <Tiisetso.Khalane@britehouse.co.za>
2023-07-27 21:07:42 +0200

    fixes

--- platform/helpers/create-postgres.sh
+++ platform/helpers/create-postgres.sh
@@ -2,9 +2,9 @@ NAME=$1
 STORAGEGB=$2
 VERSION=$3
 
-FILE_PATH=platform/gitops/${NAME}-postgres.yml
+FILE_PATH=platform/gitops/${NAME}-aws-postgres.yml
 
-cp platform/api/claim-postgres-template.yml $FILE_PATH
+cp platform/api/claim-postgres-aws-template.yml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
 yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
 yq --inplace ".spec.parameters.storageGB = \"${STORAGEGB}\"" $FILE_PATH
