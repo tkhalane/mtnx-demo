619667e8f8569d3976c374ced0e95787cff83ce7
Tiisetso.Khalane <Tiisetso.Khalane@britehouse.co.za>
2023-07-29 15:56:49 +0200

    fix input parameter types

--- platform/helpers/create-postgres.sh
+++ platform/helpers/create-postgres.sh
@@ -6,5 +6,5 @@ FILE_PATH=platform/resources/${NAME}-aws-postgres.yml
 
 cp platform/api/claim-postgres-aws-template.yml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
-yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
 yq --inplace ".spec.parameters.storageGB = \"${STORAGEGB}\"" $FILE_PATH
+yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
