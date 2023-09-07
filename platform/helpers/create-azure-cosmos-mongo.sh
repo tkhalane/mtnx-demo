4c04d3457f58fd57b454939c29dccb24c87cbfb8
Tiisetso.Khalane <Tiisetso.Khalane@britehouse.co.za>
2023-09-07 20:26:40 +0200

    tf

--- platform/helpers/create-azure-cosmos-mongo.sh
+++ platform/helpers/create-azure-cosmos-mongo.sh
@@ -1,9 +1,9 @@
 NAME=$1
-T=$2
-THROUGHPUT=$((T))
+THROUGHPUT=$(echo "$2" | awk '{print $0+0}')
 
 FILE_PATH=platform/resources/${NAME}-cosmos-mongo.yaml
 
 cp platform/api/claim-cosmos-mongo-azure-template.yml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
 yq --inplace ".spec.throughput = \"${THROUGHPUT}\"" $FILE_PATH
+yq --inplace ".spec.version = $2" $FILE_PATH
